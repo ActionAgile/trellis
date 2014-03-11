ea85103a9d96f0cb50b0d5d020e61fc758e23d8a
Ben Hughes <bwghughes@gmail.com>
2014-03-11 09:11:31 +0000

    Added sttings and gitignore

---
+++ settings.py
@@ -0,0 +1,5 @@
+APP_KEY = "d55e4d19715539f767d6aead66698b02"
+APP_TOKEN = "883856e4130c37c1c49f4ffece87b4c60277330166642db73e46dd334adf4487"
+BOARD_URL = "https://api.trello.com/1/boards/{}/lists?key={}&token={}"
+LIST_URL = "https://api.trello.com/1/lists/{}?fields=name&cards=open&card_fields=name&key={}&token={}"
+ACTION_URL = "https://api.trello.com/1/cards/{}/actions?filter=updateCard:idList,createCard&fields=date&member_fields=initials&key={}&token={}"
